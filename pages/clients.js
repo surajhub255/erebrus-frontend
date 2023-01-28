a0b9964e8f1bb2a4af9ab8f5bf9773edb4044347
ss251 <ss251@uw.edu>
2023-01-28 00:23:24 -0600

    filter clients by wallet address

--- pages/clients.js
+++ pages/clients.js
@@ -5,6 +5,8 @@ import Navbar from "../components/Navbar";
 import { ethers } from "ethers";
 import erebrusABI from "../utils/erebrusABI.json";
 import Head from "next/head";
+import Link from "next/link";
+import { motion } from "framer-motion";
 
 const Clients = () => {
   const provider = new ethers.providers.InfuraProvider(
@@ -33,17 +35,20 @@ const Clients = () => {
       const fetchData = async () => {
         setIsLoading1(true);
         const response1 = await fetch("/api/getClients");
-        const data1 = await response1.json();
+        let data1 = await response1.json();
         console.log(data1);
-        setApiData1(data1.clients);
+        data1 = data1.clients.filter((obj) => obj.WalletAddress === address);
+        console.log(address);
+        console.log(data1);
+        setApiData1(data1);
 
-        const apiCalls = data1.clients.map((item) =>
-          fetch(`/api/getClientConfig?UUID=${item.UUID}`)
-        );
-        const responses = await Promise.all(apiCalls);
-        const data2 = await Promise.all(responses.map((res) => res.json()));
-        setApiData2(data2);
-        console.log(data2);
+        // const apiCalls = data1.clients.map((item) =>
+        //   fetch(`/api/getClientConfig?UUID=${item.UUID}`)
+        // );
+        // const responses = await Promise.all(apiCalls);
+        // const data2 = await Promise.all(responses.map((res) => res.json()));
+        // setApiData2(data2);
+        // console.log(data2);
         setIsLoading1(false);
       };
 
@@ -72,11 +77,10 @@ const Clients = () => {
       </Head>
       <Navbar />
       {isOwned ? (
-        <div className="container flex flex-wrap w-100 justify-center items-center mx-auto px-4 bg-black text-white lg:h-screen pt-8 pb-8">
+        <div className="container flex flex-wrap w-100 justify-center lg:items-center items-start mx-auto px-4 bg-black text-white h-screen pt-8 pb-8 mt-8 lg:-mt-8">
           {isLoading1 ? (
             <div className="bg-black h-screen">Loading...</div>
-          ) : (
-            apiData1 &&
+          ) : apiData1 ? (
             apiData1.map((item1, index) => (
               <div className="mb-12 " key={item1.UUID}>
                 <div className="flex flex-col">
@@ -87,26 +91,21 @@ const Clients = () => {
                   </div>
                   <div className="card-body">
                     <p className="mt-2 text-gray-700 w-40 mb-4">{item1.UUID}</p>
-                    {apiData2 && apiData2[index] ? (
-                      <div>
-                        <QRCode value={apiData2[index]} />
-                        <div className="mt-4 mr-20">
-                          <a
-                            href={`data:application/octet-stream,${encodeURIComponent(
-                              apiData2[index]
-                            )}`}
-                            download="vpn.conf"
-                            className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg "
-                          >
-                            Download config file
-                          </a>
-                        </div>
-                      </div>
-                    ) : null}
                   </div>
                 </div>
               </div>
             ))
+          ) : (
+            <div className="flex flex-col justify-center text-white bg-black">
+              <p className="mb-4">You have not created any clients yet.</p>
+              <motion.div
+                initial={{ y: 100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1, transition: { duration: 1 } }}
+                className="bg-blue-500 text-white font-bold py-4 px-6 rounded-lg "
+              >
+                <Link href="/demo">Create new client</Link>
+              </motion.div>
+            </div>
           )}
         </div>
       ) : (
