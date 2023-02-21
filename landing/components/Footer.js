afa6a35ea9dfdae1cef404d6f190f833093eb00c
ss251 <ss251@uw.edu>
2023-02-21 01:53:10 -0600

    add twitter link to footer and resize contact section

--- landing/components/Footer.js
+++ landing/components/Footer.js
@@ -1,13 +1,19 @@
-import { FaFacebook, FaYoutube, FaGithub, FaDiscord } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaYoutube,
+  FaGithub,
+  FaDiscord,
+  FaTwitter,
+} from "react-icons/fa";
 import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="bg-[#1A202C]">
       <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
-        <div className="mt-4 flex justify-between">
+        <div className=" flex justify-between">
           <div>
-            <h3 className="text-xl font-semibold tracking-wide text-gray-200">
+            <h3 className="lg:text-xl text-md font-semibold tracking-wide text-gray-200">
               Follow Us
             </h3>
             <ul className="mt-4 flex mr-4">
@@ -18,6 +24,15 @@ const Footer = () => {
                   className="text-2xl hover:text-[#4267B2]"
                 >
                   <FaFacebook />
+                </Link>
+              </li>
+              <li className="mr-4">
+                <Link
+                  href="https://twitter.com/LazarusNetwork"
+                  target={"_blank"}
+                  className="text-2xl hover:text-[#1DA1F2]"
+                >
+                  <FaTwitter />
                 </Link>
               </li>
               <li className="mr-4">
@@ -41,7 +56,7 @@ const Footer = () => {
             </ul>
           </div>
           <div>
-            <h3 className="text-xl font-semibold tracking-wide text-gray-200">
+            <h3 className="lg:text-xl text-md font-semibold tracking-wide text-gray-200">
               Subscribe for updates
             </h3>
             <form className="mt-4 flex">
@@ -49,12 +64,12 @@ const Footer = () => {
                 type="email"
                 name="email"
                 id="email"
-                className="px-4 py-2 rounded-l-md focus:outline-none"
+                className="lg:px-4 md:px-4 px-2.5 py-2 rounded-l-md text-xs lg:text-base md:text-base focus:outline-none w-[75%] lg:w-full md:w-full"
                 placeholder="Email"
               />
               <button
                 type="submit"
-                className="bg-blue-500 text-white px-4 rounded-r-md"
+                className="bg-blue-500 text-xs lg:text-base md:text-base text-white lg:px-4 md:px-4 px-2 rounded-r-md w-[40%]"
               >
                 Subscribe
               </button>
@@ -65,7 +80,7 @@ const Footer = () => {
           <p className="order-2 sm:order-1">
             Erebrus 2023 &copy; All rights reserved.
           </p>
-          <div className="flex items-center order-1 sm:order-2 text-gray-600">
+          <div className="flex items-center order-1 sm:order-2 text-gray-600 lg:mr-48 md:mr-48">
             <p>Contact us on</p>
             <Link
               href="https://discord.gg/cj2Xt2u5Pj"
