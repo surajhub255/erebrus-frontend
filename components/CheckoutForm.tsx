60c50d35de49c32cbbf335ec9b98b0ca34d8d676
Punarv Pawade <punarvpawade17@gmail.com>
2024-01-31 02:27:31 +0530

    checkoutForm:fix error

--- components/CheckoutForm.tsx
+++ components/CheckoutForm.tsx
@@ -75,8 +75,9 @@ export default function CheckoutForm() {
 
     setIsLoading(false);
   };
+
   const paymentElementOptions = {
-    layout: "tabs",
+    layout: "tabs" as const,
   };
 
   return (
