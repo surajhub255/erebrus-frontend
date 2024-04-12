c983c6e3440e28e6438fcb10f5f29d37c729916b
Devsi <grace.emily876@gmail.com>
2024-04-12 21:39:44 +0530

    landing page hero section

--- pages/index.js
+++ pages/index.js
@@ -24,38 +24,46 @@ Anonymous Virtual Private Network for accessing internet in stealth mode bypassi
         <title>Erebrus</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div style={{backgroundImage: 'radial-gradient(ellipse at top left, #040819 20%, #0162FF 40% , #1D73FF, #5696FF, #8EB9FF, #AACBFF)'}} className="py-32">
-      {/* <div className="container mx-auto py-10">
-        <div className="flex flex-col items-center justify-start lg:h-full md:mt-16 mt-8 lg:ml-0 lg:mr-0 md:ml-0 md:mr-0 ml-4 mr-4 mb-36 lg:mb-0 ">
+
+<div className="min-h-screen" style={{ backgroundImage: `url('/landingtop.png')`, 
+                        backgroundColor:"black", 
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        backgroundRepeat: "no-repeat"}}>
+      <div className="container mx-auto pt-20 md:w-[80%] lg:w-[80%]">
+        <div className="flex flex-col items-center justify-start lg:h-full md:mt-16 mt-8 mb-36">
           <motion.h1
             initial={{ y: 100, opacity: 0 }}
             animate={{ y: 0, opacity: 1, transition: { duration: 1 } }}
-            className="text-5xl font-bold text-gray-300 mb-6 lg:text-center md:text-center md:w-[60%]"
+            className="text-6xl font-smeibold text-white mb-6 mr-auto w-3/5"
           >
-            Where Decentralization meets <br></br> VPN for ultimate internet security
+            Building a Resilient Decentralized Network 
           </motion.h1>
           <motion.h1
             initial={{ y: 100, opacity: 0 }}
             animate={{ y: 0, opacity: 1, transition: { duration: 1 } }}
-            className="text-md text-white mb-8 lg:w-[35%] lg:text-center md:text-center md:w-[60%]"
+            className="text-xl text-white mb-8 w-3/5 mr-auto"
           >
             <p>
-              Anonymous Virtual Private Network for accessing internet in
-              stealth mode bypassing filewalls and filters
+            Get on board with Erebrus DEPIN for a stable, decentralized internet.
+Earn rewards and enhance your connectivity
             </p>
           </motion.h1>
           <motion.div
             initial={{ y: 100, opacity: 0 }}
             animate={{ y: 0, opacity: 1, transition: { duration: 1 } }}
-            className="text-black font-bold py-3 px-14 rounded-full bg-white" 
-            style={{backgroundImage: 'linear-gradient(#FFFFFF00, #0099FF)'}}
+            className="text-black font-bold py-3 px-14 rounded-full mr-auto" style={{backgroundColor:'#E3EEFF'}}
+            // style={{backgroundImage: 'linear-gradient(#FFFFFF00, #0099FF)'}}
           >
             <Link href="/plans">
-              Subscribe Now
+              Get Started
             </Link>
           </motion.div>
         </div>
-      </div> */}
+      </div>
+      </div>
+
+      <div style={{backgroundImage: 'radial-gradient(ellipse at top left, #040819 20%, #0162FF 40% , #1D73FF, #5696FF, #8EB9FF, #AACBFF)'}} className="py-10">
 
       <Features />
       <DepinCarousel />
