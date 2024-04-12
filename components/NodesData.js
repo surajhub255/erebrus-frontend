1a49efc331e8e2daa33f5ded7404836fcf790fef
Devsi <grace.emily876@gmail.com>
2024-04-13 01:24:48 +0530

    node table

--- components/NodesData.js
+++ components/NodesData.js
@@ -43,33 +43,99 @@ const NodesData = () => {
       id="howto"
       className="flex flex-col items-center justify-start scroll-mt-16 lg:scroll-mt-0 min-h-screen"
     >
-      <div className="mb-2 font-figtree w-[70%] text-left text-gray-200">
+      <div className="font-figtree text-left text-gray-200 w-full">
         {/* <h1 className="font-bold text-4xl lg:mb-16 mb-12 lg:mt-36 text-left">
           Erebrus Nodes Data
         </h1> */}
-        <div className="p-20 rounded-xl text-black mt-20"  style={{backgroundColor:'#8EB9FF'}}>
+        <div className="text-white" style={{backgroundColor:'#202434'}}>
+
+          <div className="flex uppercase">
+            <div className="flex gap-4 w-1/3 justify-center items-center p-6" style={{border:'solid 1px #FFFFFF66'}}>
+              <img src="/nodetable1.png" className="w-20 h-20"/>
+              <div>
+                <div>No. of Nodes</div>
+                <div className="text-3xl">{"000000"}</div>
+              </div>
+            </div>
+            <div className="flex gap-4 w-1/3 justify-center items-center p-6" style={{border:'solid 1px #FFFFFF66'}}>
+              <img src="/nodetable2.png" className="w-14 h-14"/>
+              <div>
+                <div>No. of Regions</div>
+                <div className="text-3xl">{"000000"}</div>
+              </div>
+            </div>
+            <div className="flex gap-4 w-1/3 justify-center items-center p-6" style={{border:'solid 1px #FFFFFF66'}}>
+              <img src="/nodetable3.png" className="w-20 h-12"/>
+              <div>
+                <div>Active Nodes</div>
+                <div className="text-3xl">{"000000"}</div>
+              </div>
+            </div>
+          </div>
         <table className="w-full text-center">
       <thead>
         <tr>
-          <th>Node ID</th>
-          <th>Node Name</th>
-          <th>Region</th>
-          <th>Network Speed</th>
-          <th>Latency</th>
-          <th>Uptime</th>
-          <th>Last Pinged</th>
+          <th style={{border:'solid 1px #FFFFFF66'}}>
+            <div className="flex gap-4 justify-center items-center pt-2 pb-10 px-4">
+              <img src="/nodetable4.png" className="w-12 h-12"/>
+              <div>NODE ID</div>
+            </div>
+            </th>
+            <th style={{border:'solid 1px #FFFFFF66'}}>
+            <div className="flex gap-4 justify-center items-center pt-2 pb-10 px-4">
+              <img src="/nodetable5.png" className="w-10 h-10"/>
+              <div>NODE NAME</div>
+            </div>
+            </th>
+            <th style={{border:'solid 1px #FFFFFF66'}}>
+            <div className="flex gap-4 justify-center items-center pt-2 pb-10 px-4">
+              <img src="/nodetable6.png" className="w-10 h-10"/>
+              <div>REGION</div>
+            </div>
+            </th>
+            <th style={{border:'solid 1px #FFFFFF66'}}>
+            <div className="flex gap-4 justify-center items-center pt-2 pb-10 px-4">
+              <img src="/nodetable7.png" className="w-10 h-10"/>
+              <div>NETWORK SPEED</div>
+            </div>
+            </th>
+            <th style={{border:'solid 1px #FFFFFF66'}}>
+            <div className="flex gap-4 justify-center items-center pt-2 pb-10 px-4">
+              <img src="/nodetable8.png" className="w-10 h-10"/>
+              <div>LATENCY</div>
+            </div>
+            </th>
+            <th style={{border:'solid 1px #FFFFFF66'}}>
+            <div className="flex gap-4 justify-center items-center pt-2 pb-10 px-4">
+              <img src="/nodetable9.png" className="w-10 h-10"/>
+              <div>UPTIME</div>
+            </div>
+            </th>
+          {/* <th>Last Pinged</th> */}
         </tr>
       </thead>
       <tbody>
         {nodesdata.map((node) => (
-          <tr style={{paddingTop:'20px'}} key={node.id}>
-            <td style={{paddingTop:'20px'}}>{node.id}</td>
-            <td style={{paddingTop:'20px'}}>{node.nodeName}</td>
-            <td style={{paddingTop:'20px'}}>{node.region}</td>
-            <td style={{paddingTop:'20px'}}>{node.networkSpeed}</td>
-            <td style={{paddingTop:'20px'}}>{node.latency}</td>
-            <td style={{paddingTop:'20px'}}>{node.uptime}</td>
-            <td style={{paddingTop:'20px'}}>{node.lastPinged}</td>
+          <tr key={node.id}>
+            <td style={{border:'solid 1px #FFFFFF66'}}>
+              <div className="flex gap-4 justify-center items-center py-10 px-4">{node.id.slice(0, 4)}...{node.id.slice(-4)}</div>
+              </td>
+            <td style={{border:'solid 1px #FFFFFF66'}}>
+              <div className="flex gap-4 justify-center items-center py-10 px-4">{node.nodeName}</div>
+              </td>
+            <td style={{border:'solid 1px #FFFFFF66'}}>
+              <div className="flex gap-4 justify-center items-center py-10 px-4">{node.region}</div>
+              </td>
+            <td style={{border:'solid 1px #FFFFFF66'}}>
+              <div className="flex gap-4 justify-center items-center py-10 px-4">{node.networkSpeed}</div>
+              </td>
+            <td style={{border:'solid 1px #FFFFFF66'}}>
+              <div className="flex gap-4 justify-center items-center py-10 px-4">{node.latency}</div>
+              </td>
+            <td style={{border:'solid 1px #FFFFFF66'}}>
+              <div className="flex gap-4 justify-center items-center py-10 px-4">{node.uptime}</div>
+              </td>
+            {/* <td style={{paddingTop:'20px'}}>{node.lastPinged}</td> */}
           </tr>
         ))}
       </tbody>
