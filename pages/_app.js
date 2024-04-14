34a6944f8a024dd99735afb49f750cee68051de1
Devsi <grace.emily876@gmail.com>
2024-04-14 21:29:47 +0530

    paseto button

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,17 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { AuthProvider } from "../AuthContext";
 import { AppContext } from "../components/AppContext";
+import Cookies from 'js-cookie';
+import { useState } from "react";
 import '@aptos-labs/wallet-adapter-ant-design/dist/index.css'
 
 const activeChainId = ChainId.Mumbai;
 
 export default function App({ Component, pageProps }) {
+
+  const [copied, setCopied] = useState(false);
+  const paseto = Cookies.get("erebrus_token");
+
   return (
     <AppContext>
     <ThirdwebProvider desiredChainId={activeChainId}>
@@ -17,6 +23,18 @@ export default function App({ Component, pageProps }) {
         <div className="bg-black">
           <Navbar />
           <Component {...pageProps} />
+          { paseto && (<div className="flex gap-2 justify-end">
+          {copied && <p className="text-white pt-6">Paseto Copied!</p>}
+          <button className="rounded-full px-10 py-2 mb-4 mr-4 mt-4 text-white" style={{backgroundColor:'#0162FF'}}
+          onClick={() => {
+            navigator.clipboard.writeText(
+              paseto ? paseto : ''
+            );
+            setCopied(true);
+          }}
+          >Copy Paseto
+          </button>
+          </div>)}
         </div>
         {/* <Footer /> */}
       </AuthProvider>
