fc47ce73a14dd13d7c13c01e317d562f5a0f8d47
Rushikesh Nimkar <rushikeshnimkar396@gmail.com>
2024-05-14 11:27:04 +0530

    fix sui wallet zk login

--- components/Navbar.js
+++ components/Navbar.js
@@ -376,9 +376,8 @@ const Navbar = ({ isHome }) => {
       }
     }
   };
-
   const onSignMessageSui = async () => {
-    if (sendableSui) {
+    if (sendableSui && wallet.account?.publicKey.length !=0) {
       if (chainsym == "sui" && wallet.chain.name == "Sui Testnet" ) {
       try {
         const REACT_APP_GATEWAY_URL = process.env.NEXT_PUBLIC_GATEWAY_URL;
@@ -449,11 +448,86 @@ const Navbar = ({ isHome }) => {
         setshowsignbutton(true);
       }  
   }
+}
+else if (sendable && wallet.account?.publicKey.length == 0){
+  if (chainsym == "sui" && wallet.chain.name == "Sui Testnet" ) {
+    try {
+      const REACT_APP_GATEWAY_URL = process.env.NEXT_PUBLIC_GATEWAY_URL;
+      const { data } = await axios.get(
+        `${REACT_APP_GATEWAY_URL}api/v1.0/flowid?walletAddress=${wallet.address}&chain=sui`
+      );
+      console.log("address", wallet.address);
+      const msg = data.payload.eula + data.payload.flowId;
+      const nonce = data.payload.flowId;
+      // convert string to Uint8Array
+      const msgBytes = new TextEncoder().encode(msg);
+
+      const result = await wallet.signPersonalMessage({
+        message: msgBytes,
+      });
+      console.log("signature", result.signature);
+      console.log("publickey", wallet.account?.publicKey);
+      // verify signature with publicKey and SignedMessage (params are all included in result)
+      const verifyResult = await wallet.verifySignedMessage(
+        result,
+        wallet.account.publicKey
+      );
+      if (!verifyResult) {
+        console.log(
+          "signPersonalMessage succeed, but verify signedMessage failed"
+        );
+      } else {
+        console.log(
+          "signPersonalMessage succeed, and verify signedMessage succeed!"
+        );
+      }
+
+      const payload = {
+        message: message,
+        nonce: nonce,
+      };
+
+      const authenticationData = {
+        flowId: nonce,
+        signatureSui: result.signature,
+      };
+      console.log("adaddasdasd", result.signature);
+
+      const authenticateApiUrl = `${REACT_APP_GATEWAY_URL}api/v1.0/authenticate/NonSign`;
+
+      const config = {
+        url: authenticateApiUrl,
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: authenticationData,
+      };
+
+      const authResponse = await axios(config);
+      console.log("auth data", authResponse.data);
+
+      const token = await authResponse?.data?.payload?.token;
+      const userId = await authResponse?.data?.payload?.userId;
+
+      Cookies.set("erebrus_token", token, { expires: 7 });
+      Cookies.set("erebrus_wallet", suiAccount?.address ?? "", { expires: 7 });
+      Cookies.set("erebrus_userid", userId, { expires: 7 });
+
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+      setshowsignbutton(true);
+    }  
+}
+
+}
 else {
-      alert(`Switch to ${wallet.chain.name} in your wallet`);
+      alert(`Switch to Sui Testnet in your wallet`);
     }
-  }
+  
   };
+
 
   const handleDeleteCookie = () => {
     Cookies.remove("erebrus_wallet");
