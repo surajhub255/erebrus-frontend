d2b6f9340c10e8546204d053826d85ce5cb7c542
Rushikesh Nimkar <rushikeshnimkar396@gmail.com>
2024-06-12 15:00:58 +0530

    fix

--- components/Login/googleLogin.js
+++ components/Login/googleLogin.js
@@ -8,5 +8,5 @@ const CLIENT_SECRET= process.env.NEXT_PUBLIC_CLIENT_SECRET_GOOGLE_WEB2;
 export const handleLoginClick = () => {
     const state = Math.random().toString(36).substring(7);
     const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=openid%20profile%20email&state=${state}`;
-    window.location.href = authUrl,("/");
+    window.location.href = authUrl;
   };
