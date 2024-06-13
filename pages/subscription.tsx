12b1e5494794af8b0546d05fdcc631ac569381ee
Rushikesh Nimkar <rushikeshnimkar396@gmail.com>
2024-06-13 16:58:45 +0530

    fix

--- pages/subscription.tsx
+++ pages/subscription.tsx
@@ -665,7 +665,7 @@ const Subscription = () => {
     { id: 'US',region: 'US'},
     { id: 'JP', region: 'JP'},
     {id: 'CA', region: 'CA'},
-    {id: 'FL', region: 'Fl'},
+    {id: 'FL', region: 'FL'},
     {id: 'GB', region: 'GB'},
     // Add more nodes as needed
   ];
