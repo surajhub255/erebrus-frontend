6b88fc92dab0007813b713624f2c74e1d93f243e
Rushikesh Nimkar <rushikeshnimkar396@gmail.com>
2024-07-15 02:19:42 +0530

    build fix

--- pages/_app.js
+++ pages/_app.js
@@ -18,8 +18,7 @@ import {MantaPacific} from "../components/manta.ts"
 import {Peaq} from "../components/peaq.ts"
 import '../styles/globals.css';
 
-
-
+const activeChainId = ChainId.Mumbai;
 
 const queryClient = new QueryClient();
 const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID;
@@ -31,7 +30,7 @@ const metadata = {
   icons: ["https://avatars.githubusercontent.com/u/37784886"],
 };
 
-
+// const chains = [polygonAmoy];
 const chains = [MantaPacific,Peaq];
 const config = defaultWagmiConfig({
   chains,
@@ -53,20 +52,38 @@ export default function App({ Component, pageProps }) {
   return (
    
     <AppContext>
-     
+      <ThirdwebProvider desiredChainId={activeChainId}>
         <AuthProvider>
           <WagmiProvider config={config}>
             <QueryClientProvider client={queryClient}>
               <div className="bg-black">
                 <Navbar />
                 <Component {...pageProps} />
-                
+                {/* {paseto && (
+                  <div className="flex gap-2 justify-end">
+                    {copied && (
+                      <p className="text-white pt-6">Paseto Copied!</p>
+                    )}
+                    <button
+                      className="rounded-full px-10 py-2 mb-4 mr-4 mt-4 text-white"
+                      style={{ backgroundColor: "#0162FF" }}
+                      onClick={() => {
+                        navigator.clipboard.writeText(paseto ? paseto : "");
+                        setCopied(true);
+                      }}
+                    >
+                      Copy Paseto
+                    </button>
+                  </div>
+                )} */}
               </div>
               {/* <Footer /> */}
             </QueryClientProvider>
           </WagmiProvider>
         </AuthProvider>
-  
+        </ThirdwebProvider>
     </AppContext>
   );
 }
+
+
