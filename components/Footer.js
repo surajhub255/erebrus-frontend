d2c8c12e8bf856f7747e2c27e5ad75d4f46c0cf4
Devsi <grace.emily876@gmail.com>
2024-07-18 20:16:29 +0530

    footer mobile ui

--- components/Footer.js
+++ components/Footer.js
@@ -11,9 +11,9 @@ const Footer = () => {
   return (
     <footer className="bg-[#040819]">
       <div className="max-w-[90%] mx-auto py-12 px-4 overflow-hidden">
-        <div className=" flex justify-between">
+        <div className=" flex flex-wrap gap-10 justify-between">
           
-          <div className="w-1/3">
+          <div className="lg:w-1/3 md:w-1/3">
           <div className="text-white">
             <img src="/Erebrus_logo_wordmark.png" alt="Logo" className="w-48" />
             <div className="mt-4 text-sm text-gray-400">Redefining digital connectivity and unleashing the future of internet with globally accessible, secure and private network through the power of DePIN.</div>
