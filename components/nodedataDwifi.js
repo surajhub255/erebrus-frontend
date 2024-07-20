6180d92cf1d6e0b0f89266e4b9668fdc33467fcc
Devsi <grace.emily876@gmail.com>
2024-07-20 15:23:42 +0530

    dwifi explorer ui

--- components/nodedataDwifi.js
+++ components/nodedataDwifi.js
@@ -47,9 +47,171 @@ const NodeDwifiStream = () => {
   }, []);
 
   return (
-    <div className='text-white'>
-      <h1>NodeDwifi Stream Data</h1>
-      <table>
+    <div className='text-white px-3'>
+
+
+<div className="overflow-x-auto">
+            <table className="min-w-full bg-black rounded-lg">
+            <thead style={{ height: "10px" }}>
+              <tr>
+                <th style={{ border: "solid 1px #FFFFFF66" }}>
+                  <div className="flex gap-4 justify-center items-center pt-4 pb-4 px-4">
+                    <img src="/nodetable4.png" className="w-12 h-12" />
+                    <div>Gateway</div>
+                  </div>
+                </th>
+                <th style={{ border: "solid 1px #FFFFFF66" }}>
+                  <div className="flex gap-4 justify-center items-center pt-4 pb-4 px-4">
+                    <img src="/nodetable5.png" className="w-10 h-10" />
+                    <div>Created At</div>
+                  </div>
+                </th>
+                {/* <th style={{ border: "solid 1px #FFFFFF66" }}>
+                  <div className="flex gap-4 justify-center items-center pt-4 pb-4 px-4">
+                    <img src="/nodetable5.png" className="w-10 h-10" />
+                    <div>MAC Address</div>
+                  </div>
+                </th> */}
+                <th style={{ border: "solid 1px #FFFFFF66" }}>
+                  <div className="flex gap-4 justify-center items-center pt-4 pb-4 px-4">
+                    <img src="/nodetable6.png" className="w-10 h-10" />
+                    <div>IP Address</div>
+                  </div>
+                </th>
+                <th style={{ border: "solid 1px #FFFFFF66" }}>
+                  <div className="flex gap-4 justify-center items-center pt-4 pb-4 px-4">
+                    <img src="/nodetable7.png" className="w-10 h-10" />
+                    <div>Connected At</div>
+                  </div>
+                </th>
+                <th style={{ border: "solid 1px #FFFFFF66" }}>
+                  <div className="flex gap-4 justify-center items-center pt-4 pb-4 px-4">
+                    <img src="/nodetable8.png" className="w-10 h-10" />
+                    <div>Total Connected Time</div>
+                  </div>
+                </th>
+                <th style={{ border: "solid 1px #FFFFFF66" }}>
+                  <div className="flex gap-4 justify-center items-center pt-4 pb-4 px-4">
+                    <img src="/nodetable9.png" className="w-10 h-10" />
+                    <div>Connected</div>
+                  </div>
+                </th>
+                <th style={{ border: "solid 1px #FFFFFF66" }}>
+                  <div className="flex gap-4 justify-center items-center pt-4 pb-4 px-4">
+                    <img src="/nodetable9.png" className="w-10 h-10" />
+                    <div>Last Checked</div>
+                  </div>
+                </th>
+                {/* <th style={{ border: "solid 1px #FFFFFF66" }}>
+                  <div className="flex gap-4 justify-center items-center pt-4 pb-4 px-4">
+                    <img src="/nodetable7.png" className="w-10 h-10" />
+                    <div>Default Gateway</div>
+                  </div>
+                </th> */}
+                <th style={{ border: "solid 1px #FFFFFF66" }}>
+                  <div className="flex gap-4 justify-center items-center pt-4 pb-4 px-4">
+                    <img src="/nodetable8.png" className="w-10 h-10" />
+                    <div>Manufacturer</div>
+                  </div>
+                </th>
+                <th style={{ border: "solid 1px #FFFFFF66" }}>
+                  <div className="flex gap-4 justify-center items-center pt-4 pb-4 px-4">
+                    <img src="/nodetable9.png" className="w-10 h-10" />
+                    <div>Interface Name</div>
+                  </div>
+                </th>
+                <th style={{ border: "solid 1px #FFFFFF66" }}>
+                  <div className="flex gap-4 justify-center items-center pt-4 pb-4 px-4">
+                    <img src="/nodetable9.png" className="w-10 h-10" />
+                    <div>Host SSID</div>
+                  </div>
+                </th>
+              </tr>
+            </thead>
+              <tbody>
+              {data.map((item) => (
+            item.status.map((device, index) => (
+              <tr key={`${item.id}-${index}`}>
+                {index === 0 && (
+                  <>
+                    {/* <td rowSpan={item.status.length}>{item.id}</td> */}
+                    <td rowSpan={item.status.length}
+                    style={{ border: "solid 1px #FFFFFF66" }}>
+                    <div className="flex gap-4 justify-center items-center py-2 px-4">
+                       {item.gateway}
+                      </div>
+                      </td>
+                    <td rowSpan={item.status.length}
+                    style={{ border: "solid 1px #FFFFFF66" }}>
+                    <div className="flex gap-4 justify-center items-center py-2 px-4">
+                      {new Date(item.created_at).toLocaleString()}
+                      </div>
+                      </td>
+                    {/* <td rowSpan={item.status.length}
+                    style={{ border: "solid 1px #FFFFFF66" }}>
+                    <div className="flex gap-4 justify-center items-center py-2 px-4">
+                      {new Date(item.updated_at).toLocaleString()}
+                      </div>
+                      </td> */}
+                  </>
+                )}
+                {/* <td style={{ border: "solid 1px #FFFFFF66" }}>
+                <div className="flex gap-4 justify-center items-center py-2 px-4">
+                {device.macAddress}
+                </div>
+                </td> */}
+                <td style={{ border: "solid 1px #FFFFFF66" }}>
+                <div className="flex gap-4 justify-center items-center py-2 px-4">
+                  {device.ipAddress}
+                  </div>
+                  </td>
+                <td style={{ border: "solid 1px #FFFFFF66" }}>
+                <div className="flex gap-4 justify-center items-center py-2 px-4">
+                  {new Date(device.connectedAt).toLocaleString()}
+                  </div></td>
+                <td style={{ border: "solid 1px #FFFFFF66" }}>
+                <div className="flex gap-4 justify-center items-center py-2 px-4">{device.totalConnectedTime}
+                  </div>
+                </td>
+                <td style={{ border: "solid 1px #FFFFFF66" }}>
+                <div className="flex gap-4 justify-center items-center py-2 px-4">
+                  {device.connected.toString()}
+                  </div>
+                  </td>
+                <td style={{ border: "solid 1px #FFFFFF66" }}>
+                <div className="flex gap-4 justify-center items-center py-2 px-4">
+                  {new Date(device.lastChecked).toLocaleString()}
+                  </div>
+                </td>
+                {/* <td style={{ border: "solid 1px #FFFFFF66" }}>
+                <div className="flex gap-4 justify-center items-center py-2 px-4">
+                  {device.defaultGateway}
+                  </div>
+                  </td> */}
+                <td style={{ border: "solid 1px #FFFFFF66" }}>
+                <div className="flex gap-4 justify-center items-center py-2 px-4">
+                  {device.manufacturer}
+                  </div>
+                  </td>
+                <td style={{ border: "solid 1px #FFFFFF66" }}>
+                <div className="flex gap-4 justify-center items-center py-2 px-4">
+                  {device.interfaceName}
+                  </div>
+                  </td>
+                <td style={{ border: "solid 1px #FFFFFF66" }}>
+                <div className="flex gap-4 justify-center items-center py-2 px-4">
+                  {device.hostSSID}
+                  </div>
+                  </td>
+              </tr>
+            ))
+          ))}
+            </tbody>
+            </table>
+          </div>
+
+
+      {/* <table>
         <thead>
           <tr>
             <th>ID</th>
@@ -94,7 +256,7 @@ const NodeDwifiStream = () => {
             ))
           ))}
         </tbody>
-      </table>
+      </table> */}
     </div>
   );
 };
