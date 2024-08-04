3911e61f63cde8063ffbfe46216841639f1a1690
Rushikesh Nimkar <rushikeshnimkar396@gmail.com>
2024-08-04 18:10:49 +0530

    usernode page update

--- pages/usernodes.tsx
+++ pages/usernodes.tsx
@@ -14,7 +14,7 @@ const Dwifi = () => {
             backgroundBlendMode: 'overlay',
           }}
           >
-      <div className="flex flex-col items-center justify-center lg:h-full mt-10 lg:ml-0 lg:mr-0 md:ml-0 md:mr-0 ml-4 mr-4 mb-36 lg:mb-0 px-20 py-40">
+      <div className="flex flex-col items-center justify-center lg:h-[40vh] mt-10 lg:ml-0 lg:mr-0 md:ml-0 md:mr-0 ml-4 mr-4 mb-36 lg:mb-0 px-20 py-40">
         <motion.h1
           initial={{ y: 100, opacity: 0 }}
           animate={{ y: 0, opacity: 1, transition: { duration: 1 } }}
